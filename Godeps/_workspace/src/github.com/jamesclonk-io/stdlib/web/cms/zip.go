b1cf23ee31448c3aba20e0aa7b45dc9c466f93d7
JamesClonk <jamesclonk@jamesclonk.ch>
2015-05-27 22:15:24 +0200

    latest stdlib

--- Godeps/_workspace/src/github.com/jamesclonk-io/stdlib/web/cms/zip.go
+++ Godeps/_workspace/src/github.com/jamesclonk-io/stdlib/web/cms/zip.go
@@ -12,10 +12,12 @@ import (
 
 func (c *CMS) readZipFromURL() (map[string][]byte, error) {
 	resp, err := http.Get(c.Input)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -55,9 +57,9 @@ func (c *CMS) readZip(data *bytes.Reader, size int64) (map[string][]byte, error)
 		if err != nil {
 			return nil, err
 		}
-		defer rc.Close()
 
 		data, err := ioutil.ReadAll(rc)
+		rc.Close()
 		if err != nil {
 			return nil, err
 		}
